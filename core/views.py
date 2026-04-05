a818ee5662840b37760618066abc0203f2b6d4e0
Tiago Fernandes <tiagoferprojects@gmail.com>
2026-04-04 21:14:23 -0300

    feat: Redirecionar usuários para alguns casos

--- core/views.py
+++ core/views.py
@@ -25,6 +25,9 @@ def home(request):
 
 def registro_contratante_view(request):
     """Registro específico para Contratante"""
+    if request.user.is_authenticated:
+        return redirect('home')
+
     if request.method == 'POST':
         # Faz uma cópia mutável do POST e força a role
         data = request.POST.copy()
@@ -44,6 +47,10 @@ def registro_contratante_view(request):
 
 def registro_trabalhador_view(request):
     """Registro específico para Trabalhador"""
+
+    if request.user.is_authenticated:
+        return redirect('home')
+
     if request.method == 'POST':
         # Faz uma cópia mutável do POST e força a role
         data = request.POST.copy()
@@ -63,6 +70,10 @@ def registro_trabalhador_view(request):
 
 def login_view(request):
     """Login de usuário"""
+
+    if request.user.is_authenticated:
+        return redirect('home')
+
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
